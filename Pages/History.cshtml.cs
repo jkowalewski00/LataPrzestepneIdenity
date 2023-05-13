7610cee3ef7b99b11d3f2408f6d7b7afcd5f4707
kevvol <jakubkowalewski48@gmail.com>
2023-05-13 19:04:16 +0200

    Dodaj pliki projektów.

---
+++ Pages/History.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LataPrzestepneIdenity.Data;
+using LataPrzestepneIdenity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace LataPrzestepneIdenity.Pages
+{
+    [Authorize]
+    public class HistoryModel : PageModel
+    {
+        private readonly LataPrzestepneIdenity.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HistoryModel(LataPrzestepneIdenity.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        
+        public IList<LeapYears> AddedData { get; set; }
+
+        public string CurrentUser;
+
+        public void OnGet()
+        {
+            AddedData = _context.LeapYears.ToList();
+            CurrentUser = _userManager.GetUserId(User);
+        }
+
+        public IActionResult OnPost()
+        {
+            int id = Convert.ToInt32(Request.Form["id"]);
+
+            var data = _context.LeapYears.SingleOrDefault(d => d.Id == id);
+
+           if(data != null)
+            {
+                _context.LeapYears.Remove(data);
+                _context.SaveChanges();
+                RedirectToAction("/History");
+                OnGet();
+            }
+
+           return Page();
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using LataPrzestepneIdenity.Data;
+using LataPrzestepneIdenity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace LataPrzestepneIdenity.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public LeapYears LeapYears { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+
+        public String? Name { get; set; }
+
+        public String AlertMessage { get; set; }
+
+        public ClaimsPrincipal GetUser()
+        {
+            return User;
+        }
+
+        public IActionResult OnPost(ClaimsPrincipal user)
+        {
+            if(LeapYears.BirthYear == 0)
+            {
+                ModelState.Remove("LeapYears.BirthYear");
+                ModelState.AddModelError("LeapYears.BirthYear", "Pole Rok urodenia jest wymagane");
+            }
+            
+            LeapYears.CheckYear();
+            LeapYears.UserID = _userManager.GetUserId(User);
+            LeapYears.UserName = _userManager.GetUserName(User);
+            LeapYears.Date = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            _context.LeapYears.Add(LeapYears);
+            _context.SaveChanges();
+
+            if (LeapYears.LeapYear)
+            {
+                AlertMessage = $"{LeapYears.FirstName} urodził się w {LeapYears.BirthYear} roku. To był rok przestępny.";
+
+            }
+            else
+            {
+                AlertMessage = $"{LeapYears.FirstName} urodził się w {LeapYears.BirthYear} roku. To nie był rok przestępny.";
+            }
+            return Page();
+        }
+
+        public void OnGet()
+        {
+
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+
+        }
+    }
+}
